757a99b32e164e8bc20537952ca11060c66bc030
Marcelo Melo <marcelovmelo201@gmail.com>
2024-11-10 17:25:17 -0300

    Demo: atualizar dados

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,60 +1,38 @@
 package application;
 
 import db.DB;
-import db.DbException;
 
 import java.sql.*;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+
 
 public class Main {
     public static void main(String[] args) {
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
-        Connection conn; //Conecta com o banco
+        Connection conn = null;
         PreparedStatement st = null;
 
         try{
             conn = DB.getConnection();
 
             st = conn.prepareStatement(
-                        "INSERT INTO seller "
-                            + "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
-                            + "VALUES "
-                            + "(?, ?, ?, ?, ?)",
-                    Statement.RETURN_GENERATED_KEYS);
+                    "UPDATE seller "
+                    + "SET BaseSalary = BaseSalary + ? "
+                    + "WHERE "
+                    + "(DepartmentId = ?)");
 
-            st.setString(1, "Carl Purple");
-            st.setString(2, "carl@gmail.com");
-            st.setDate(3, new Date(sdf.parse("22/04/1985").getTime()));
-            st.setDouble(4, 3000.0);
-            st.setInt(5, 4);
+            st.setDouble(1, 200);
+            st.setInt(2, 2);
 
             int rowsAffected = st.executeUpdate();
 
-            if(rowsAffected > 0){
-                //Código para pegar o id automático
-                ResultSet rs = st.getGeneratedKeys();
+            System.out.println("Done! Rows affected "+ rowsAffected);
 
-                while(rs.next()){
-                    int id = rs.getInt(1);
-                    System.out.println("Done! Id = "+ id);
-                }
-            }
-            else {
-                System.out.println("No rows affected.");
-            }
         }
         catch (SQLException e){
-            throw new DbException(e.getMessage());
-        }
-        catch (ParseException e){
             e.printStackTrace();
-        }
-        finally {
+        } finally {
             DB.closeStatement(st);
             DB.closeConnection();
         }
-
     }
 }
